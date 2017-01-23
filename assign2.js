dbd99a23b6e362ee636dc8a251d08bfcac5a6849
Rmalhi <ramanmalhi286@yahoo.co.in>
2017-01-23 23:05:31 +0000

    first commit

---
+++ assign2.js
@@ -0,0 +1,8 @@
+const myVal = process.argv;
+let arr2 = [];
+arr2.push(myVal[2]);
+console.log(arr2);
+arr2.push(Number(myVal[2]));
+arr2.push(Number(myVal[3]));
+let a = Number(myVal[2])+Number(myVal[3]);
+console.log(a);
